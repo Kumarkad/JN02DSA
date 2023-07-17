955eb4946bb475c8a095be49daf39642b6ec0768
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-07-17 07:28:20 +0530

    Add files via upload

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -1,60 +1,56 @@
-/******************************************************************************
-Program to demonstrate Linked List
+//Program to demonstrate Singly Linked List
 
-*******************************************************************************/
 #include <iostream>
-
-using namespace std;
-class Node{
+class Node
+{
     public:
-    int data;
-    Node *next;
+        int data;
+        Node *next;
     
-    public:
-     Node(int no){
-         data=no;
-         next=NULL;
-     }
+        Node(int no)
+        {
+            data=no;
+            next=NULL;
+        }
 };
-
-class LinkedList{
+class LinkedList
+{
     Node *head;
-    public :
+    public:
     LinkedList()
     {
         head=NULL;
     }
     void create(){
         int no;
-        Node *newnode,*t;
-        cout<<"Enter the number upto 999 : ";
-        cin>>no;
-        while(no!=999){
+        Node *newnode, *t;
+        std::cout<<"Enter the numbers upto 999 : "  ;
+        std::cin>>no;
+        while(no!=999)
+        {
             newnode=new Node(no);
-            if(head==NULL)
+            if (head==NULL)
                 head=newnode;
-            else{
+            else
+            {
                 t=head;
-                while(t->next!=NULL){
+                while(t->next!=NULL)
                     t=t->next;
-                    t->next=newnode;
-                }
+                t->next=newnode;
             }
-             cin>>no;
-           
+            std::cin>>no;
         }
-        
     }
-    
     void display(){
         Node *t=head;
+        if (head==NULL)
+            std::cout<<"\nList is Empty.....";
         while(t!=NULL)
         {
-            cout<<t->data<<"->";
+            std::cout<<t->data<<"->";
             t=t->next;
         }
     }
-    
     void insertFirst(int no)
     {
         Node *newnode;
@@ -62,74 +58,93 @@ class LinkedList{
         newnode->next=head;
         head=newnode;
     }
-    
-     
     void insertLast(int no)
     {
-        Node *newnode,*t;
+        Node *newnode, *t;
         newnode=new Node(no);
         t=head;
         while(t->next!=NULL)
             t=t->next;
         t->next=newnode;
     }
-    
     void deleteFirst()
     {
         Node *t;
         t=head;
         head=head->next;
+        std::cout<<"\n"<<t->data<<" is deleting....";
         delete t;
-        
     }
-    
     void deleteLast()
     {
-       Node *p,*t;
-       t=head;
-       while(t->next!=NULL)
-       {
-           p=t;
-           t=t->next;
-       }
-       p->next=NULL;
-       delete t;
+        Node *t, *p;
+        t=head;
+        p=t;
+        while(t->next!=NULL)
+        {
+            p=t;
+            t=t->next;
+        }
+        p->next=NULL;
+        std::cout<<"\n"<<t->data<<" is deleting....";
+        delete t;
     }
-    
-    void insertAfterGivenNode(int no,int n)
+    void insertAfterGivenNode(int no, int n)
     {
-       Node *newnode,*t;
-       newnode=new Node(no);
-       if(head==NULL)
+        Node *newnode,*t;
+        newnode=new Node(no);
+        if (head==NULL)
+        {
+            std::cout<<"***************";
             head=newnode;
-       else
-       {
-           t=head;
-           while(t->data!=n && t!=NULL)
-           {
-               t=t->next;
-           }
-           if (t==NULL)
-           
-               cout<<"Element not present in List !!! ";
-           else
-           {
-               newnode->next=t->next;
-               
-           }
-       }
+        }
+        else
+        {
+            t=head;
+            while(t->data!=n && t!=NULL)
+            {
+                t=t->next;
+            }
+            if (t==NULL)
+                std::cout<<"Given node not present in the list...";
+            else
+            {
+                newnode->next=t->next;
+                t->next=newnode;
+            }
+        }
     }
+
 };
-
-int main()
-{
-    cout<<"------- Singly Linked List -------"<<endl;
-    LinkedList ls;
-    ls.create();
-    cout<<"List is as follows : ";
-    ls.display();
-    ls.insertFirst(20);
-    ls.display();
-
+int main() {
+    int no,n;
+    std::cout << "--------------Singly LinkedList-------------!";
+    LinkedList list;
+    list.create();
+    std::cout<<"\nList is as follows : \n";
+    list.display();
+    std::cout<<"Enter the number to be inserted at the begining : ";
+    std::cin>>no;
+    list.insertFirst(no);
+    std::cout<<"\nList is as follows : \n";
+    list.display();
+    std::cout<<"Enter the number to be inserted at the End : ";
+    std::cin>>no;
+    list.insertLast(no);
+    std::cout<<"\nList is as follows : \n";
+    list.display();
+    list.deleteFirst();
+    std::cout<<"\nList is as follows : \n";
+    list.display();
+    list.deleteLast();
+    std::cout<<"\nList is as follows : \n";
+    list.display();
+    std::cout<<"Enter the number to be inserted : ";
+    std::cin>>no;
+    std::cout<<"Enter the number after which newnode to be inserted : ";
+    std::cin>>n;
+    list.insertAfterGivenNode(no,n);
+    std::cout<<"\nList is as follows : \n";
+    list.display();
     return 0;
 }
